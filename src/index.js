e16527e677093e25d761ebc81b5909cd5ebb283b
Anthony Hinsinger <anthony.hinsinger@univ-pau.fr>
2016-12-12 20:40:57 +0100

    Used customResolvers to improve Apollo cache usage

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
-import ApolloClient from 'apollo-client';
+import ApolloClient, { toIdValue } from 'apollo-client';
 import Kapton from 'kapton';
 import gql from 'graphql-tag';
 import { createFakeNetworkInterface } from './network';
 
+// define unique id of User type.
+const dataIdFromObject = (result) => {
+  if (result.__typename === "User") {
+    return result.__typename+'_'+result.uid;
+  }
+  return null;
+};
+
 // Create the apollo client
 const apolloClient = new ApolloClient({
   // In a normal app, you'll probably use createNetworkInterface instead.
-  // This demo app use an in-client graphql enpoint
+  // This demo app use an in-client graphql enpoint and simulate latency
   networkInterface: createFakeNetworkInterface({ latency: 1000 }),
-  // define unique id of User type.
-  dataIdFromObject: (result) => {
-    if (result.__typename === "User") {
-      return result.__typename+'_'+result.uid;
+  dataIdFromObject,
+  customResolvers: {
+    Query: {
+      user: (_, args) => toIdValue(dataIdFromObject({ __typename: 'User', uid: args['uid'] })),
     }
-    return null;
   }
 });
 
-// Get a behavior "factory"
+// Get a Kapton behavior "factory"
 const graphql = Kapton({apolloClient});
 
 // Queries and mutations
